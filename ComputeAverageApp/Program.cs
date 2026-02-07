e332cf62a408098437af2d66aed1bd63bede792b
Herrera-JP <j0hnp4tr1ck23@gmail.com>
2026-02-07 18:52:18 +0800

    Add files via upload

---
+++ ComputeAverageApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            double g1, g2, g3, g4, g5;
+            double average;
+            double rounded;
+
+            Console.WriteLine("Enter 5 grades separated by new line:");
+
+            g1 = Convert.ToDouble(Console.ReadLine());
+            g2 = Convert.ToDouble(Console.ReadLine());
+            g3 = Convert.ToDouble(Console.ReadLine());
+            g4 = Convert.ToDouble(Console.ReadLine());
+            g5 = Convert.ToDouble(Console.ReadLine());
+
+            average = (g1 + g2 + g3 + g4 + g5) / 5;
+            rounded = Math.Round(average);
+
+            Console.WriteLine("The average is " + average + " and round off to " + rounded);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
