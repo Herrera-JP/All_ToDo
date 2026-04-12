d1a5e9b94caa2bf28051f9bc0b01375d4e65ddc9
Herrera-JP <j0hnp4tr1ck23@gmail.com>
2026-04-12 22:23:30 +0800

    Add files via upload

---
+++ Herrera_StudentGradeAnalyzer/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Herrera_StudentGradeAnalyzer
+{
+    partial class studentgradefrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(studentgradefrm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtStudentNumber = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.txtPrelim = new System.Windows.Forms.TextBox();
+            this.txtMidterm = new System.Windows.Forms.TextBox();
+            this.txtPreFinal = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.Submit = new System.Windows.Forms.Button();
+            this.studentData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.studentData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(606, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(19, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Student Grade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(51, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Prelim Grade";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(51, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 26);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Midterm Grade";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(51, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 26);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Prefinal Grade";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(51, 327);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 26);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Final Grade";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtStudentNumber
+            // 
+            this.txtStudentNumber.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtStudentNumber.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentNumber.Location = new System.Drawing.Point(198, 111);
+            this.txtStudentNumber.Multiline = true;
+            this.txtStudentNumber.Name = "txtStudentNumber";
+            this.txtStudentNumber.Size = new System.Drawing.Size(152, 29);
+            this.txtStudentNumber.TabIndex = 9;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentName.Location = new System.Drawing.Point(198, 146);
+            this.txtStudentName.Multiline = true;
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(152, 29);
+            this.txtStudentName.TabIndex = 10;
+            // 
+            // txtPrelim
+            // 
+            this.txtPrelim.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrelim.Location = new System.Drawing.Point(228, 227);
+            this.txtPrelim.Name = "txtPrelim";
+            this.txtPrelim.Size = new System.Drawing.Size(100, 27);
+            this.txtPrelim.TabIndex = 11;
+            // 
+            // txtMidterm
+            // 
+            this.txtMidterm.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMidterm.Location = new System.Drawing.Point(228, 260);
+            this.txtMidterm.Name = "txtMidterm";
+            this.txtMidterm.Size = new System.Drawing.Size(100, 27);
+            this.txtMidterm.TabIndex = 12;
+            // 
+            // txtPreFinal
+            // 
+            this.txtPreFinal.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPreFinal.Location = new System.Drawing.Point(228, 293);
+            this.txtPreFinal.Name = "txtPreFinal";
+            this.txtPreFinal.Size = new System.Drawing.Size(100, 27);
+            this.txtPreFinal.TabIndex = 13;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFinal.Location = new System.Drawing.Point(228, 326);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(100, 27);
+            this.txtFinal.TabIndex = 14;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(192, 364);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Submit.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Submit.Location = new System.Drawing.Point(273, 364);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(100, 36);
+            this.Submit.TabIndex = 17;
+            this.Submit.Text = "Compute";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // studentData
+            // 
+            this.studentData.BackgroundColor = System.Drawing.Color.DarkSlateGray;
+            this.studentData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentData.Location = new System.Drawing.Point(379, 90);
+            this.studentData.Name = "studentData";
+            this.studentData.RowHeadersWidth = 51;
+            this.studentData.RowTemplate.Height = 24;
+            this.studentData.Size = new System.Drawing.Size(533, 330);
+            this.studentData.TabIndex = 18;
+            // 
+            // studentgradefrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(924, 453);
+            this.Controls.Add(this.studentData);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtPreFinal);
+            this.Controls.Add(this.txtMidterm);
+            this.Controls.Add(this.txtPrelim);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.txtStudentNumber);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "studentgradefrm";
+            this.Text = "studentgradefrm";
+            this.Click += new System.EventHandler(this.studentgradefrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.studentData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtStudentNumber;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.TextBox txtPrelim;
+        private System.Windows.Forms.TextBox txtMidterm;
+        private System.Windows.Forms.TextBox txtPreFinal;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.DataGridView studentData;
+    }
+}
+
---
+++ Herrera_StudentGradeAnalyzer/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herrera_StudentGradeAnalyzer
+{
+    public partial class studentgradefrm : Form
+    {
+        DataTable table = new DataTable("table");
+
+        public struct StudentData
+        {
+            public string StudentNumber;
+            public string Name;
+            public double Prelim;
+            public double Midterm;
+            public double PreFinal;
+            public double Final;
+            public double Average;
+            public string Remarks;
+        }
+
+        public studentgradefrm()
+        {
+            InitializeComponent();
+        }
+
+        private void studentgradefrm_Load(object sender, EventArgs e)
+        {
+            
+            table.Columns.Add("Student Number", typeof(long));
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Prelim", typeof(double));
+            table.Columns.Add("Midterm", typeof(double));
+            table.Columns.Add("Pre-Final", typeof(double));
+            table.Columns.Add("Final", typeof(double));
+            table.Columns.Add("Average", typeof(double));
+            table.Columns.Add("Remarks", typeof(string));
+
+            studentData.DataSource = table;
+
+            studentData.EnableHeadersVisualStyles = false;
+            studentData.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(44, 90, 108);
+            studentData.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            studentData.DefaultCellStyle.BackColor = Color.FromArgb(44, 50, 90);
+            studentData.DefaultCellStyle.ForeColor = Color.White;
+            studentData.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(54, 60, 100);
+            studentData.RowHeadersVisible = false;
+            studentData.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput(out double p, out double m, out double pf, out double f))
+            {
+                StudentData s = new StudentData
+                {
+                    StudentNumber = txtStudentNumber.Text,
+                    Name = txtStudentName.Text,
+                    Prelim = p,
+                    Midterm = m,
+                    PreFinal = pf,
+                    Final = f
+                };
+
+                s.Average = CalculateAverage(p, m, pf, f);
+                s.Remarks = GetRemarks(s.Average);
+
+                AddStudentRecord(s);
+            }
+        }
+
+        public bool ValidateInput(out double p, out double m, out double pf, out double f)
+        {
+            p = m = pf = f = 0;
+
+            if (string.IsNullOrWhiteSpace(txtStudentNumber.Text) ||
+                string.IsNullOrWhiteSpace(txtStudentName.Text) ||
+                string.IsNullOrWhiteSpace(txtPrelim.Text) ||
+                string.IsNullOrWhiteSpace(txtMidterm.Text) ||
+                string.IsNullOrWhiteSpace(txtPreFinal.Text) ||
+                string.IsNullOrWhiteSpace(txtFinal.Text))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return false;
+            }
+            if (txtStudentName.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show("Name should not contain numbers.");
+                return false;
+            }
+
+            if (!long.TryParse(txtStudentNumber.Text, out _))
+            {
+                MessageBox.Show("Invalid Student Number.");
+                return false;
+            }
+
+            if (!double.TryParse(txtPrelim.Text, out p) ||
+                !double.TryParse(txtMidterm.Text, out m) ||
+                !double.TryParse(txtPreFinal.Text, out pf) ||
+                !double.TryParse(txtFinal.Text, out f))
+            {
+                MessageBox.Show("Grades must be numbers.");
+                return false;
+            }
+
+            if (p < 0 || p > 100 || m < 0 || m > 100 ||
+                pf < 0 || pf > 100 || f < 0 || f > 100)
+            {
+                MessageBox.Show("Grades must be between 0 and 100.");
+                return false;
+            }
+
+            return true;
+        }
+
+
+        public double CalculateAverage(double a, double b)
+        {
+            return (a + b) / 2;
+        }
+
+        public double CalculateAverage(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
+
+        public double CalculateAverage(double a, double b, double c, double d)
+        {
+            return (a + b + c + d) / 4;
+        }
+
+        public static string GetRemarks(double avg)
+        {
+            return avg >= 75 ? "Passed" : "Failed";
+        }
+
+        public void AddStudentRecord(StudentData s)
+        {
+            table.Rows.Add(
+                s.StudentNumber,
+                s.Name,
+                s.Prelim,
+                s.Midterm,
+                s.PreFinal,
+                s.Final,
+                s.Average,
+                s.Remarks
+            );
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStudentNumber.Clear();
+            txtStudentName.Clear();
+            txtPrelim.Clear();
+            txtMidterm.Clear();
+            txtPreFinal.Clear();
+            txtFinal.Clear();
+
+            table.Rows.Clear();
+            txtStudentNumber.Focus();
+        }
+
+        
+    }
+}
